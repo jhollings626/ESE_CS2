b3068e0227618ed204038eb2bf631481e6a85bba
Jack Hollingsworth <jackhhollings@gmail.com>
2023-10-30 20:06:17 -0500

    fix omicron script

--- matlab/secondRange.m
+++ matlab/secondRange.m
@@ -3,12 +3,12 @@ load("COVID_STL.mat");
 
 n = 6; %variable for dimensionality of A
 
-A = [ %here's the matrix
+A = [
     0.999922 0.000000 0.040 0.005 0.000 0.000;
     0.000000 0.999980 0.000 0.020 0.000 0.000;
     0.000078 0.000000 0.953 0.005 0.000 0.000; 
     0.000000 0.000020 0.000 0.960 0.000 0.000; 
-    0.000000 0.000000 0.004 0.012 1.000 0.000; 
+    0.000000 0.000000 0.001 0.003 1.000 0.000; 
     0.000078 0.000020 0.000 0.000 0.000 1.000;  
 ];
 
@@ -21,19 +21,24 @@ percentNormal = 1 - percentAtRisk;
 dailyDates = linspace(dates(1),dates(end),length(dates)*7); %create 158*7 daily dates spanning the range of virus propagation
 startDateIndex = 473; %index of the first date of the rnage
 endDateIndex = 592; %index of the last date of the range
+weekIndexSTART = round(startDateIndex / 7); %for indexing into dates
+weekIndexEND = round(endDateIndex /7 ); %for indexing into dates
 startDate = dailyDates(startDateIndex); %get datetime formatted start date
 endDate = dailyDates(endDateIndex); %get the datetime formatted last date
-d = endDateIndex; %number of days to simulate for, equal to the final index of the date range, or the date number
-startingNormal = cases_STL(68) * 0.86;
-startingVulnerable = cases_STL(68) * 0.14;
+d = endDateIndex - startDateIndex; %number of days to simulate for, equal to the final index of the date range, or the date number
+startingNormalInfected = cases_STL(weekIndexSTART) * 0.9533; %normal cases are the rest of the non-vulnerable cases
+startingDeaths = deaths_STL(weekIndexSTART);
+startingVulnerableInfected = cases_STL(weekIndexSTART) * 0.0467; %vulnerable cases should be 1/3 of 14% of the total population
+
 
 x0 = [
     (POP_STL * percentNormal);
     (POP_STL * percentAtRisk); 
-    startingNormal; 
-    startingVulnerable;
-    0;
-    0
+    startingNormalInfected; 
+    startingVulnerableInfected;
+    startingDeaths;
+    startingVulnerableInfected + startingNormalInfected%total starting cases is the sum of 
+    % starting normal infected and starting vulnerable infected data
  ];
 
 sys_sir_base = ss(A,B,eye(n) ,zeros(n,1),1);
@@ -53,21 +58,29 @@ ylim auto; hold off;
 
 %casesFraction = cases_STL / POP_STL; %create new case vector storing cases as fraction of whole population
 %plot(casesFraction(1:100));
-firstWeek = round(startDateIndex/7); %find index of firstWeek in cases_STL
-lastWeek = round(endDateIndex/7); %find index of lastWeek in cases_STL
 
 figure;
 hold on;
-plot(dailyDates(startDateIndex:endDateIndex),origY(startDateIndex:d,n) / POP_STL); %startDate:d takes indices of the desired range
-plot(dates(firstWeek:lastWeek),cases_STL(firstWeek:lastWeek) / POP_STL); %need to build this such taht it is same length as number of days that we want to store so we can plot them together
-
+plot(dailyDates(startDateIndex:endDateIndex - 1),origY(1:d,5) / POP_STL);
 plot(dates(firstWeek:lastWeek),deaths_STL(firstWeek:lastWeek)/POP_STL);
-plot(dailyDates(startDateIndex:d),origY(startDateIndex:d,5) / POP_STL);
 xlim([startDate endDate]);
-legend('model','actual','actual deaths','modeled deaths');
-title('Daily Cases As Fraction of Population From 6/30/21 - 10/26/21');
+title('Total Deaths As Fraction of Population From 6/30/21 - 10/26/21');
+legend('Modeled Deaths','Actual Deaths');
 ylabel('Fraction of Population');
 xlabel('Date');
+
+figure;
+hold on;
+plot(dailyDates(startDateIndex:endDateIndex -1),origY(1:d,n) / POP_STL); %trust me it works
+plot(dates(weekIndexSTART:weekIndexEND),cases_STL(weekIndexSTART:weekIndexEND) / POP_STL); %need to build this such taht it is same length as number of days that we want to store so we can plot them together
+legend('Modeled Cases', 'Actual Cases');
+xlim([startDate endDate]);
+title('Total Cases As Fraction of Population From 6/30/21 - 10/26/21');ylabel('Fraction of Population');
+ylabel('Fraction of Population');
+xlabel('Date');
+hold off;
+
+
 
 error = 0;
 samples = 0;
@@ -76,9 +89,10 @@ for i = 1:7:d %below is used for calculating error between model and actual
     %we can also use the above count variable to access weekly entries in
     %cases_STL
     modeledCases = origY(i,6); %access a point from each week, reported on the same day as the actual data
-    actualCases = cases_STL(samples); %cases STL contains weekly data
+    actualCases = cases_STL(weekIndexSTART); %cases STL contains weekly data
     tempError = ((modeledCases - actualCases) / actualCases) * 100; %calculate weekly error
     error = error + tempError;
+    weekIndexSTART = weekIndexSTART + 1; %for indexing into weekly data
 end
 
 error = error/samples;
